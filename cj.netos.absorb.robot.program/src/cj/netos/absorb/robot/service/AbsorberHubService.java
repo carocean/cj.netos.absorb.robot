5d3d9a4fdde3db6957b6fc7146094c5cd93e6c54
cj <cj@ss.com>
2020-09-05 22:52:13 +0800

    1.0.0

--- cj.netos.absorb.robot.program/src/cj/netos/absorb/robot/service/AbsorberHubService.java
+++ cj.netos.absorb.robot.program/src/cj/netos/absorb/robot/service/AbsorberHubService.java
@@ -236,8 +236,8 @@ public class AbsorberHubService implements IAbsorberHubService {
         }
         TailBill bill = new TailBill();
         bill.setPerson(principal);
-        bill.setAmount(amount);
-        bill.setBalance(tails.subtract(amount));
+        bill.setAmount(amount.multiply(new BigDecimal("-1.00")).setScale(RobotUtils.BIGDECIMAL_SCALE,RoundingMode.DOWN));
+        bill.setBalance(tails.add(amount));
         bill.setOrder(2);
         bill.setRefsn(null);
         bill.setBankid(bankid);
